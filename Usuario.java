799e414438d06dc2f4c247fd8d0bff6a2b69ef31
Emi34521 <166149620+Emi34521@users.noreply.github.com>
2024-08-24 18:33:46 -0600

    Agregue los constructores, getters y setters a cada clase

--- Usuario.java
+++ Usuario.java
@@ -5,5 +5,48 @@ public class Usuario {
     private String contraseña; 
     private Banco banco; 
     private Articulo articulos[];
+    public Usuario(String nombre, double gasto, String contraseña, Banco banco, Articulo[] articulos) {
+        this.nombre = nombre;
+        this.gasto = gasto;
+        this.contraseña = contraseña;
+        this.banco = banco;
+        this.articulos = articulos;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getGasto() {
+        return gasto;
+    }
+    public void setGasto(double gasto) {
+        this.gasto = gasto;
+    }
+    public double getAhorro() {
+        return ahorro;
+    }
+    public void setAhorro(double ahorro) {
+        this.ahorro = ahorro;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    public Banco getBanco() {
+        return banco;
+    }
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+    public Articulo[] getArticulos() {
+        return articulos;
+    }
+    public void setArticulos(Articulo[] articulos) {
+        this.articulos = articulos;
+    }
 
 }
