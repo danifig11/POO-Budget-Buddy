3ec8b5a653fc120d7499fb0ee443a94dfa220dda
Emi34521 <166149620+Emi34521@users.noreply.github.com>
2024-08-24 18:18:11 -0600

    Aqui solo cree las clases con sus atributos, todavia me falta agregar metodo basicos

--- Usuario.java
+++ Usuario.java
@@ -1,53 +1,9 @@
-import java.util.ArrayList;
-
 public class Usuario {
     private String nombre; 
     private double gasto; 
     private double ahorro; 
-    private String contraseña;
-    private ArrayList <Articulo> articulos = new ArrayList<>();
-    
-
-    public Usuario(String nombre, double gasto, String contraseña) {
-        this.nombre = nombre;
-        this.gasto = gasto;
-        this.contraseña = contraseña;
-    }
-    
-    public Usuario(String nombre, String contraseña) {
-        this.nombre = nombre;
-        this.contraseña = contraseña;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public double getGasto() {
-        return gasto;
-    }
-    public void setGasto(double gasto) {
-        this.gasto = gasto;
-    }
-    public double getAhorro() {
-        return ahorro;
-    }
-    public void setAhorro(double ahorro) {
-        this.ahorro = ahorro;
-    }
-    public String getContraseña() {
-        return contraseña;
-    }
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-    public ArrayList<Articulo> getArticulos() {
-        return articulos;
-    }
-    public void setArticulos(Articulo articulo) {
-        this.articulos.add(articulo);
-    }
+    private String contraseña; 
+    private Banco banco; 
+    private Articulo articulos[];
 
 }
