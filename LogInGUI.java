09b014caf7105102643e23994eb138a00b2a93a4
Adrian-24914 <pen24914@uvg.edu.gt>
2024-09-23 16:08:40 -0600

    Creación del espacio para agregar el usuario y la contraseña. Aún no funcionan jiji

--- LogInGUI.java
+++ LogInGUI.java
@@ -11,17 +11,36 @@ public class LogInGUI {
 
         JPanel panel = new JPanel();
         JFrame ventana = new JFrame();
-        ventana.setSize(500, 200);
+        ventana.setSize(600, 250);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setVisible(true); 
+         
         ventana.add(panel);
 
         panel.setLayout(null);
 
+        //Ingresar el usuario, después cambio el tag
         JLabel tag = new JLabel("Usuario");
-        tag.setBounds(10, 20, 80, 25);
+        tag.setBounds(10, 20, 80, 20);
         panel.add(tag);
+        //Esta weonada se supone que lee texto pero no sé cómo guardar el imput aún
+        JTextField textoUsuario = new JTextField(20);
+        textoUsuario.setBounds(100, 20, 165, 25);
+        panel.add(textoUsuario);
 
+        //Aquí la contraseña
+        JLabel tagContrasena = new JLabel("Contraseña");
+        tagContrasena.setBounds(10, 50, 80, 20);
+        panel.add(tagContrasena);
+
+        JPasswordField password = new JPasswordField(); //En vez de que sea texto, son puntitos que lindo uwu
+        password.setBounds(100, 50, 165, 25);
+        panel.add(password);
+
+
+        
+        
+        
+        ventana.setVisible(true);
 
     }
 }
