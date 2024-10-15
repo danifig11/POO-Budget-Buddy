af66abf60adc9c3f40fbfd7674a64497c770c898
Adrian-24914 <pen24914@uvg.edu.gt>
2024-10-15 14:28:47 -0600

    Cambio en el GUI para que se puedan implementar diferentes JPanels y que no todos los procesos sean en el main

--- LogInGUI.java
+++ LogInGUI.java
@@ -1,46 +1,27 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LogInGUI {
-    //Por lo que entiendo este debería ser el Main porque es la interfaz que se presentará al Usuario
-    //Todo va a ser pura prueba y error en esta cosa, por eso el Branch jiji
+    private HashMap<String, String> users = new HashMap<>();
 
-    public static void main(String[] args) {
-
-        JPanel panel = new JPanel();
-        JFrame ventana = new JFrame();
-        ventana.setSize(600, 250);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
-        ventana.add(panel);
-
-        panel.setLayout(null);
-
-        //Ingresar el usuario, después cambio el tag
-        JLabel tag = new JLabel("Usuario");
-        tag.setBounds(10, 20, 80, 20);
-        panel.add(tag);
-        //Esta weonada se supone que lee texto pero no sé cómo guardar el imput aún
-        JTextField textoUsuario = new JTextField(20);
-        textoUsuario.setBounds(100, 20, 165, 25);
-        panel.add(textoUsuario);
-
-        //Aquí la contraseña
-        JLabel tagContrasena = new JLabel("Contraseña");
-        tagContrasena.setBounds(10, 50, 80, 20);
-        panel.add(tagContrasena);
-
-        JPasswordField password = new JPasswordField(); //En vez de que sea texto, son puntitos que lindo uwu
-        password.setBounds(100, 50, 165, 25);
-        panel.add(password);
-
-
-        
-        
-        
-        ventana.setVisible(true);
-
+    public LoginApp() {
+        cargarUsuarios(); // Carga el archivo CSV con los usuarios y los datos
+        crearLoginFrame(); // Crea la interfaz del Login
     }
+    
+    BufferedReader md = new BufferedReader(new InputStreamReader(System.in));
+    ArrayList<Usuario> usuarios = GestorCSV.cargarUsuarios("usuarios.csv"); // Cargar usuarios desde CSV
+    boolean logueado = false;
+    String rutaArchivo = "usuarios.csv"; 
+
+    
+
 }
