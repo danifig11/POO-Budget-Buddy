c3cca16217375ef3ce590bb40b98a9e0cd0774fe
Adrian-24914 <pen24914@uvg.edu.gt>
2024-10-15 22:59:37 -0600

    cambios pequeños

--- MainGUI.java
+++ MainGUI.java
@@ -1,5 +1,30 @@
+import javax.swing.*;
+
 public class MainGUI {
+
+    private JFrame frame;
+
     public static void main(String[] args) {
-        new LogInGUI(); // Crear la aplicación de login
+        SwingUtilities.invokeLater(() -> {
+            MainGUI main = new MainGUI();
+            main.showLogIn();
+        });
     }
+
+    public MainGUI(){
+        frame = new JFrame("Budget_Buddy");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300); // Tamaño de la ventana
+        frame.setLocationRelativeTo(null); // Centrar la ventana
+    }
+
+    public void showLogIn(){
+        //JPanel de LogIn se añade al JFrame
+        LogInGUI LogIn = new LogInGUI(this);
+        frame.setContentPane(LogIn);
+        frame.setVisible(true);
+    }
+
+
+
 }
