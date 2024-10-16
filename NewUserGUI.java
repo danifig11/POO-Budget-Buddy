9be8f2103ed035cb24afeca4192e001cc1af1208
Adrian-24914 <pen24914@uvg.edu.gt>
2024-10-15 18:20:45 -0600

    Pequeños cambios en el login y NewUser

--- NewUserGUI.java
+++ NewUserGUI.java
@@ -1,13 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class NewUserGUI extends JPanel {
     private JTextField userText;
     private JPasswordField passwordText;
     private JButton createUserButton, backButton;
 
+    BufferedReader md = new BufferedReader(new InputStreamReader(System.in));
+    ArrayList<Usuario> usuarios = GestorCSV.cargarUsuarios("usuarios.csv"); // Cargar usuarios desde CSV
+    boolean logueado = false;
+    String rutaArchivo = "usuarios.csv"; 
+
     public NewUserGUI() {
+        NewUserFrame();
+    }
+    
+    public void NewUserFrame(){
+        JFrame frame = new JFrame("Nuevo Usuario");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300); // Tamaño de la ventana
+        frame.setLocationRelativeTo(null); // Centrar la ventana
+
+        // Crear panel para el login con GridBagLayout para un diseño flexible
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(new Color(60, 63, 65)); // Fondo oscuro
+
         setLayout(new GridBagLayout());
         setBackground(new Color(60, 63, 65));
         GridBagConstraints gbc = new GridBagConstraints();
@@ -63,8 +87,29 @@ public class NewUserGUI extends JPanel {
         gbc.gridy = 5;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         add(backButton, gbc);
-    }
 
+        createUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nombre = userText.getText();
+                String contra = String.valueOf(passwordText.getPassword());
+                Usuario user = new Usuario(nombre, 0.0, contra);  // Inicialmente con gasto 0
+                usuarios.add(user);
+                GestorCSV.guardarUsuarios(usuarios, rutaArchivo);  // Guardar al archivo CSV
+            }
+        });
+
+        // Acción del botón de regresar al login
+        backButton.addActionListener(e -> {
+            frame.dispose(); // Cerrar la ventana actual
+            // Regresar a la ventana de login
+        });
+
+        // Añadir el panel al marco y hacerlo visible
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    }
+     
     
 }
 
